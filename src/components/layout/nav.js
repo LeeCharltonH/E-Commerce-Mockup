4d4d483815976f34034226956ebe0e20488d69e9
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2022-04-20 17:47:42 +0100

    Added nav component.

---
+++ src/components/layout/nav.js
@@ -0,0 +1,20 @@
+import styles from "./nav.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import { Badge } from "../../UI/badge";
+
+const Nav = () => {
+  const bagIcon = (
+    <FontAwesomeIcon icon={faBagShopping} className={styles.icon} />
+  );
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.cart}>
+        Basket {bagIcon} <Badge>1</Badge>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
