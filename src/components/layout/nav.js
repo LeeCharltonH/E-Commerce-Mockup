4c290b186892d35593dc497ea101e938983091b3
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2022-04-25 14:41:31 +0100

    Added item counter to nav basket.

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -2,17 +2,22 @@ import styles from "./nav.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import { Badge } from "../UI/badge";
+import { useSelector } from "react-redux";
 
 const Nav = (props) => {
+  const basket = useSelector((state) => state.basketReducer);
   const modalHandler = props.modal;
   const bagIcon = (
     <FontAwesomeIcon icon={faBagShopping} className={styles.icon} />
   );
+  const itemCount = basket.basket.reduce((prevVal, currentVal) => {
+    return prevVal + currentVal.quantity
+  }, 0);
 
   return (
     <nav className={styles.nav}>
       <div className={styles.cart} onClick={modalHandler}>
-        Basket {bagIcon} <Badge>1</Badge>
+        Basket {bagIcon} <Badge>{itemCount}</Badge>
       </div>
     </nav>
   );
