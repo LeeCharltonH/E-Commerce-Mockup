db822a306d323fd68925bebf469e9b6a3ed75123
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2022-04-25 10:22:10 +0100

    Added if statement to store to check if item already exists in cart.

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,35 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = {basket: []};
+const initialState = { basket: [] };
 
 const basketSlice = createSlice({
-    name: 'Basket Items',
-    initialState: initialState,
-    reducers: {
-        addItem(state, action){
-         state.basket = [...state.basket, action.payload.item]
-        },
-        removeItem(state, action){
-
-        }
-    }
+  name: "Basket Items",
+  initialState: initialState,
+  reducers: {
+    addItem(state, action) {
+      let exists = state.basket.some(
+        (item) => item.id === action.payload.item.id
+      );
+      if (exists) {
+        const index = state.basket.findIndex(object => {
+            return object.id === action.payload.item.id;
+          });
+        state.basket[index].quantity = state.basket[index].quantity + 1;
+    
+      } else {
+        state.basket = [...state.basket, action.payload.item];
+      }
+    },
+    removeItem(state, action) {},
+  },
 });
 
 export const basketActions = basketSlice.actions;
 
-const store = configureStore({reducer: {
-    basketReducer: basketSlice.reducer
-}})
-
-
+const store = configureStore({
+  reducer: {
+    basketReducer: basketSlice.reducer,
+  },
+});
 
 export default store;
